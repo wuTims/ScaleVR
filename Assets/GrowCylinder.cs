b325bafc4031a5f4abf0ae2068b7c7a89772aca4
Tim Wu <tlwu@live.unc.edu>
2016-10-27 03:40:22 -0400

    Initial commit

---
+++ Assets/GrowCylinder.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrowCylinder : MonoBehaviour {
+
+	public float height = 10f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localScale = new Vector3 (transform.localScale.x, height, transform.localScale.z);
+	}
+
+	void OnGui() {
+		
+	}
+
+	public void AdjustHeight (float newHeight){
+		height = newHeight;
+	}
+}
---
+++ Assets/SelectorScript.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+var selectedCharacter : int = 1;
+var characterName : String;
+var currentSelect : GameObject;
+
+
+function Update()
+{
+   if(Input.GetKeyDown(KeyCode.Space))
+   {
+      characterName = "water";
+      currentSelect = GameObject.Find(characterName);
+      if(currentSelect.GetComponent.<Renderer>().enabled){
+      	currentSelect.GetComponent.<Renderer>().enabled = false;
+      }else{
+      	currentSelect.GetComponent.<Renderer>().enabled = true;
+      }
+
+   }
+}
