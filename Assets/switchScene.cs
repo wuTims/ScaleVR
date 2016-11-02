5884846892897da5617100c05440d34fcf78d45a
Yongwei Zheng <zhengyw@live.unc.edu>
2016-11-02 16:38:12 -0400

    addedSwitchSceneC#

---
+++ Assets/switchScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class switchScene : MonoBehaviour {
+
+	public void onBtnClicked(int sceneToChangeTo){
+		SceneManager.LoadScene (sceneToChangeTo);
+	}
+}
