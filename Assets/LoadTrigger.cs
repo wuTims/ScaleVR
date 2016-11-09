64a0cc99376475b6f23ade6f4bee8a1a67f0e0b0
Tim Wu <tlwu@live.unc.edu>
2016-11-09 04:06:18 -0500

    Added scene loading triggers

---
+++ Assets/LoadTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadTrigger : MonoBehaviour {
+
+	public string loadName;
+	public string unloadName;
+
+	private void OnTriggerEnter (Collider col){
+		if (!loadName.Equals ("")) {
+			SceneManagerTool.Instance.Load (loadName);
+		}
+
+		if (!unloadName.Equals ("")) {
+			StartCoroutine ("UnloadScene");
+		}
+	}
+
+	IEnumerator UnloadScene(){
+		yield return new WaitForSeconds (.10f);
+		SceneManagerTool.Instance.Unload (unloadName);
+	}
+}
---
+++ Assets/SceneManagerTool.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerTool : MonoBehaviour {
+	public static SceneManagerTool Instance{ set; get; }
+
+	private void Awake() {
+		Instance = this;
+		Load ("Player");
+		Load ("Scene1");
+		//Load ("Bridge");
+		//Load ("Scene2");
+
+	}
+
+	public void Load (string sceneName) {
+		if (!SceneManager.GetSceneByName(sceneName).isLoaded) {
+			SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+		}
+	}
+
+	public void Unload(string sceneName){
+		if (SceneManager.GetSceneByName(sceneName).isLoaded) {
+			SceneManager.UnloadScene(sceneName);
+		}
+	}
+
+}
